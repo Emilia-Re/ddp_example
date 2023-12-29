5ec727f1d5f6758c73182096911af935d1464456
Emilia-Re <74725531+Emilia-Re@users.noreply.github.com>
2023-12-29 17:37:02 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,6 +13,24 @@
 
 #参考https://pytorch.org/docs/stable/elastic/run.html#launcher-api
 
+#从torch.distributed.launch 改为torchrun
+#python -m torch.distributed.launch --use-env train_script.py 改为
+# torchrun train_script.py
+
+#代码内部修改：
+#原先
+#import argparse
+#parser = argparse.ArgumentParser()
+#parser.add_argument("--local-rank", type=int)
+#args = parser.parse_args()
+
+#local_rank = args.local_rank
+
+#后来
+#import os
+#local_rank = int(os.environ["LOCAL_RANK"])
+
+
 torchrun --standalone --nnodes=1 --nproc-per-node=4 main.py
 
 
